0e7ffb5facf19f40b2dcfb1851d06bc75f9abf3f
robson.baia <robson.baia@JNV0604866.jv01.local>
2023-12-03 15:49:46 -0300

    add

--- ultralimsDesafioLais/src/ultralimsDesafioLais/ultralimsDesafioLais.java
+++ ultralimsDesafioLais/src/ultralimsDesafioLais/ultralimsDesafioLais.java
@@ -1,5 +1,27 @@
 package ultralimsDesafioLais;
 
 public class ultralimsDesafioLais {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Desafio UltraLims busca de endereço por cep");
+		
+	}
+		private int cep;
 
-}
+		public int getCep() {
+		return cep;
+	}
+
+		public void setCep(int cep) {
+		this.cep = cep;
+	}
+	
+	
+
+	}
+	
+	
+	
+	
+	
