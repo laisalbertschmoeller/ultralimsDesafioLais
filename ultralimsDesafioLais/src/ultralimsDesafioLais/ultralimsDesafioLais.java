960288136ed6257211ed902ea14333c366e807b3
robson.baia <robson.baia@JNV0604866.jv01.local>
2023-12-03 18:28:31 -0300

    comitando projeto

--- ultralimsDesafioLais/src/ultralimsDesafioLais/ultralimsDesafioLais.java
+++ ultralimsDesafioLais/src/ultralimsDesafioLais/ultralimsDesafioLais.java
@@ -2,13 +2,6 @@ package ultralimsDesafioLais;
 
 public class ultralimsDesafioLais {
 	
-	public static void main(String[] args) {
-		
-		webServiceCep webServiceCep = WebServiceCep.serachcep
-		
-		
-	}
-	
 	
 	}
 
